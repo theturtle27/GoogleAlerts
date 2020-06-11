55849392b016c9d0dbbf46b2e02e46b37241bf81
theturtle27 <62739849+theturtle27@users.noreply.github.com>
2020-06-12 06:30:31 +0800

    updated code: added another website to scrape, BoardReader
    used dataFrame to write into excel
    
    more exception catching

--- BoardReader.py
+++ BoardReader.py
@@ -2,59 +2,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
 from datetime import datetime
 from selenium.webdriver.common.keys import Keys
 import openpyxl
-import pandas as pd
 
 # path where chromedriver is located
+# PATH = r":C:\Users\yc_19\Documents\chromedriver"
 PATH = r"chromedriver"
 
 # keyword for searching
 print("Please enter Keyword: ")
 keyword = input()
-
 numberOfArticles = int(input("Please enter the number of articles: "))
 
-# get date time
 now = datetime.now()
 date = now.strftime("%d-%m-%y %H%M")
-
 # open chrome
 browser = webdriver.Chrome(PATH)
-
 # go to website
-browser.get('https://www.google.com.sg/alerts#')
+browser.get('https://boardreader.com/')
 
 # locate the search box and send keyword
 element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[1]/div[1]/div/input"))
+    EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[2]/input"))
 )
 element.send_keys(keyword)
-
-# change results to all results
-show_options = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located(
-        (By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[2]/span[3]")))
-show_options.click()
-
-show = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[1]"
-                                              "/table/tbody/tr[5]/td[2]/div/div[1]"))
-)
-show.click()
-
-show_expanded = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/div[5]/div[2]/div"))
-)
-show_expanded.click()
-
-# wait for page to load
-WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "result_title_link")))
-
-# get results and sources
-links = browser.find_elements_by_class_name("result_title_link")
-sources = browser.find_elements_by_class_name("result_source")
 
 # empty list to store info
 titlesText = []
@@ -62,47 +35,72 @@ sourcesText = []
 linksText = []
 keywordText = []
 paragraphText = []
-
-# variable to iterate through
+# i is variable for articles in current page
+# j is variable for all articles
 i = 0
-while i < numberOfArticles:
+j = 0
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "title")))
+
+# get results and sources in a list
+titles = browser.find_elements_by_class_name("title")
+sources = browser.find_elements_by_class_name("last-info")
+links = browser.find_elements_by_tag_name("a")
+
+cookies = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/table/tbody/tr/td[2]/a")))
+cookies.click()
+
+while j < numberOfArticles:
     paragraph_summary = ""
     try:
-        titlesText.append(links[i].text)
+        titlesText.append(titles[i].text)
         sourcesText.append(sources[i].text)
-        linksText.append(links[i].get_attribute("href"))
+        linksText.append(links[(i*3)+1].get_attribute("href"))
         keywordText.append(keyword)
     except Exception as e:
         print(e)
         break
     # add paragraph
     try:
-        links[i].send_keys(Keys.CONTROL + Keys.RETURN)
+        links[(i*3)+1].send_keys(Keys.CONTROL + Keys.RETURN)
         # Switch to the new window and open URL B
         browser.switch_to.window(browser.window_handles[1])
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "p")))
         paragraphs = browser.find_elements_by_tag_name("p")
         for paragraph in paragraphs:
             paragraph_summary = paragraph_summary + paragraph.text
-        print(i + 1, ' article done!')
+        print(j + 1, ' article done!')
     except Exception as e:
         print(e)
-        print(i + 1, ' article error!')
+        print(j + 1, ' article error!')
     finally:
         paragraphText.append(paragraph_summary)
         browser.close()
         browser.switch_to.window(browser.window_handles[0])
         i += 1
+        j += 1
 
-# create dataFrame
+    # next page
+    if i == 10:
+        nextPage = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div[1]/app-root/results/div/div[1]/div/div/div/p[2]/a")))
+        nextPage.click()
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "title")))
+        titles = browser.find_elements_by_class_name("title")
+        sources = browser.find_elements_by_class_name("last-info")
+        links = browser.find_elements_by_tag_name("a")
+        i = 0
+
 df = pd.DataFrame()
 df['Keyword'] = keywordText
 df['Title'] = titlesText
 df['Source'] = sourcesText
 df['Link'] = linksText
 df['Paragraph'] = paragraphText
+print(df)
 
-# look for existing file
 try:
     wb = openpyxl.load_workbook(keyword + '.xlsx')
 
@@ -122,12 +120,8 @@ try:
 
         # Save the file
         writer.save()
-# write new file
 except Exception as e:
-    print(e)
     df.to_excel(keyword + '.xlsx',
                 sheet_name=date,
                 index=False)
-
-# write to csv
 df.to_csv(keyword + date + '.csv', index=False)
